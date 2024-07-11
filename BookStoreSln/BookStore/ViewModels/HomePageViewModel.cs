4e0c22bca3430bb140cd48ccc971ca2e1f273ca5
EthanJoubert <124621733+EthanJoubert@users.noreply.github.com>
2024-07-11 23:00:48 +0200

    Search function works kinda

--- BookStoreSln/BookStore/ViewModels/HomePageViewModel.cs
+++ BookStoreSln/BookStore/ViewModels/HomePageViewModel.cs
@@ -44,5 +44,22 @@ namespace BookStore.ViewModels
                 BookName = root.books[0].title;
             }
         }
+
+        [RelayCommand]
+        public async void SearchForBook()
+        {
+            string searchkey = "programming";
+            HttpResponseMessage response = await _client.GetAsync($"https://api.itbook.store/1.0/search/{searchkey}");
+            response.EnsureSuccessStatusCode(); // Ensure success status code
+
+            string jsonResponse = await response.Content.ReadAsStringAsync();
+            Root root = JsonConvert.DeserializeObject<Root>(jsonResponse);
+
+            if (root.books != null && root.books.Count > 0)
+            {
+                BookName = root.books[0].title;
+
+            }
+        }
     }
 }
